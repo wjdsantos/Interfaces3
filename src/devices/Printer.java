a0a979be7c8a1ae8c76944e8eeae83d3ba3a1dee
Walter dos Santos <walter4asantos@gmail.com>
2020-09-10 14:29:41 -0300

    Combo Device

--- src/devices/Printer.java
+++ src/devices/Printer.java
@@ -1,17 +1,7 @@
 package devices;
 
-public class Printer extends Device {
+public interface Printer {
 
-	public Printer(String serialNumber) {
-		super(serialNumber);
-	}
-
-	@Override
-	public void processDoc(String doc) {
-		System.out.println("Printer processing: " + doc);
-	}
+	void print(String doc);
 	
-	public void print(String doc) {
-		System.out.println("Printing: " + doc);
-	}
 }
